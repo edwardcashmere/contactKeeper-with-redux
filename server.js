46b770874e2cdb67846ab2f858a49b8908253876
edwardcashmere <eomondi06@gmail.com>
2020-09-16 18:19:04 +0300

    done

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ connectMongo()
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/contacts',require('./routes/contacts'));
 app.use('/api/users',require('./routes/users'));
+
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'client','build')))
+    app.use(express.static('client/build'))
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
